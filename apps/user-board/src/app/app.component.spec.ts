dcd05c2c9d62781aee66f1cf08b0ffc0ad342be2
LAPTOP-CS40PCN5\Naveen-Nandhini <nandyngp@gmail.com>
2022-02-21 09:51:45 +0100

    chore: project cleanup and added Angular Material design package

--- apps/user-board/src/app/app.component.spec.ts
+++ apps/user-board/src/app/app.component.spec.ts
@@ -1,11 +1,14 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AppComponent, NxWelcomeComponent],
+      declarations: [AppComponent],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
+    ]
     }).compileComponents();
   });
 
@@ -13,20 +16,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'user-board'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('user-board');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome user-board'
-    );
   });
 });
